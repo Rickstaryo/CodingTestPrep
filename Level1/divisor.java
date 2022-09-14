e66efd9e58b6090b1b736cb2ea48647448695f4c
rickstaryo <qnrud16@gmail.com>
2022-09-14 20:51:11 +0900

    arraySumup

--- Level1/divisor.java
+++ Level1/divisor.java
@@ -1,24 +1,13 @@
 package Level1;
 
 public class divisor {
-    public int solution(int n) {
+    public int solution(int num) {
         int answer = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (n % i == 0) {
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0)
                 answer += i;
-            }
         }
-        return answer;
+        return answer + num;
+
     }
 }
-
-// Better Code
-// public int solution(int n) {
-// int answer = 0;
-// for (int i = 1; i < n/2; i++) {
-// if (n % i == 0) {
-// answer += i;
-// }
-// }
-// return answer;
-// }
--- Level1/oddAndEven.java
+++ Level1/oddAndEven.java
@@ -3,7 +3,7 @@ package Level1;
 class Solution {
     public String solution(int num) {
         String answer = "";
-        if (num % 2 == 0 && num == 0) {
+        if (num % 2 == 0) {
             answer = "Even";
         } else {
             answer = "Odd";
