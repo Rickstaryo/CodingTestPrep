617534e1866e6f69bce8d9d5f44f2adc705df8e4
rickstaryo <qnrud16@gmail.com>
2022-09-21 00:23:59 +0900

    2solution

---
+++ Level1/palindrome.py
@@ -0,0 +1,18 @@
+# Wrong solving python code
+# def solution(n):
+#     answer = [n.length]
+#     cnt= 0
+#     while(n>0):
+#         answer[cnt] = n%10
+#         n /= 10
+#         cnt +=1
+#     return answer
+
+
+def solution(n):
+    a = []
+
+    while n > 1:
+        a.append(int(n % 10))
+        n /= 10
+    return a
