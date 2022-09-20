d3b42a015c77a3cafbe26f826bbb5ed219fb19cf
rickstaryo <qnrud16@gmail.com>
2022-09-21 00:01:03 +0900

    1 solution

---
+++ Level1/sizer.py
@@ -0,0 +1,14 @@
+def solution(s, n):
+    #     1.일정한 순번이 있으니 리스트를 만든다
+    s = 'ssk'
+    n = 3
+    s = list(s)
+    for i in range(len(s)):
+        #         2. 분기 시켜 준다
+        if s[i].isupper():
+            #         3. 아스키코드로 바꿔준다 더해준다
+            s[i] = chr((ord(s[i]) - ord('A') + n) + ord('A'))
+        elif s[i].islower():
+            s[i] = chr((ord(s[i]) - ord('a') + n) + ord('a'))
+    print(helloworld)
+    return "".join(s)
