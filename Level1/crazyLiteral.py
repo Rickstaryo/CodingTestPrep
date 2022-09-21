57a43f0f7cec617bf339de38513aaf50f656866d
rickstaryo <qnrud16@gmail.com>
2022-09-22 08:15:45 +0900

    2 solution

---
+++ Level1/crazyLiteral.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12930
+def solution(s):
+    answer = ''
+    new_list = s.split(' ')
+    for i in new_list:
+        for j in range(len(i)):
+            if j % 2 == 0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer += ' '
+    return answer[0:-1]
--- Level1/needMoeny.py
+++ Level1/needMoeny.py
@@ -6,7 +6,7 @@ def solution(price, money, count):
         return pay-money
     else:
         return 0
-
+#  합의 계산식에서 필요한 것은 얼마나 남는게 부족한지 이다  
 
 def Bettersolution(price, money, count):
     #     등차수열 의 합공식 사용
