63538d6c5598fc2f6516debadad3a421d4fe8d73
rickstaryo <qnrud16@gmail.com>
2023-01-16 09:37:18 +0900

    one solution

---
+++ Level0_JavaScript/20230116_StringSort.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    my_string = sorted(list(my_string.lower()))
+    answer = ''.join(my_string)
+
+    return answer
+
+
+def solution(my_string):
+    return ''.join(sorted(my_string.lower()))
