7dee2d7a240ac51e2396cecda4e63f6d342e67a6
rickstaryo <qnrud16@gmail.com>
2023-02-14 13:32:24 +0900

    one solution more & upgrade

--- Level0_JavaScript/20230214_alien.py
+++ Level0_JavaScript/20230214_alien.py
@@ -4,3 +4,10 @@ def solution(age):
     for i in str(age):
         answer+=alien[int(i)]
     return answer
+
+def solution(age):
+    #alien=["a","b","c","d","e","f","g","h","i","j"]
+    #answer = ''
+    #for i in str(age):
+    #   answer+=alien[int(i)]  
+    return"".join([chr(int(i)+97) for i in str(age)])
