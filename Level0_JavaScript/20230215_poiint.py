c32c05793a135ee97ac76960ce7b0cde9d6d7482
rickstaryo <qnrud16@gmail.com>
2023-02-15 21:45:00 +0900

    one more solution

--- Level0_JavaScript/20230215_poiint.py
+++ Level0_JavaScript/20230215_poiint.py
@@ -13,3 +13,12 @@ def solution(dot):
         return 1 if x>0 else 3
     else:
         return 4 if x>0 else 2
+def solution(dot):
+    x,y = dot
+    if x*y>0:
+        return 1 if x>0 else 3
+    else:
+        return 4 if x>0 else 2
+def solution(dot):
+    quad = [(3,2),(4,1)]
+    return quad[dot[0] > 0][dot[1] > 0]
