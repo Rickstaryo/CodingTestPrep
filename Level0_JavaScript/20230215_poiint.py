80811faee8beca90329ced44fdb3eeae4419e163
rickstaryo <qnrud16@gmail.com>
2023-02-15 21:24:11 +0900

    one solution

---
+++ Level0_JavaScript/20230215_poiint.py
@@ -0,0 +1,15 @@
+def solution(dot):
+    if dot[0]>0 and dot[1]>0:
+        return 1
+    elif dot[0]>0 and dot[1]<0:
+        return 4
+    elif dot[0]<0 and dot[1]>0:
+        return 2
+    else:
+        return 3
+def solution(dot):
+    x,y = dot
+    if x*y>0:
+        return 1 if x>0 else 3
+    else:
+        return 4 if x>0 else 2
