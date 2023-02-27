48bee3021f1bc4a1ba0fb0587fd5d63fc92a70b1
rickstaryo <qnrud16@gmail.com>
2023-02-27 22:25:34 +0900

    one soluition

--- Level0_JavaScript/20230227_facotiral.py
+++ Level0_JavaScript/20230227_facotiral.py
@@ -6,3 +6,13 @@ def solution(n):
             return i-1
         elif n == answer:
             return i
+
+
+def solution(n):
+    answer = 1
+    for i in range(1, n+1):
+        answer = i*answer
+        if n < answer:
+            return i-1
+        elif n == answer:
+            return i
