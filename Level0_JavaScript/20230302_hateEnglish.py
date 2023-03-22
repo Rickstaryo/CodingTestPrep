8f218bf7277d89a53edf2a67c63f896c6a2cd540
rickstaryo <qnrud16@gmail.com>
2023-03-23 06:55:29 +0900

    one solution

---
+++ Level0_JavaScript/20230302_hateEnglish.py
@@ -0,0 +1,5 @@
+
+def solution(numbers):
+    for num, eng in enumerate(["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]):
+        numbers = numbers.replace(eng, str(num))
+    return int(numbers)
