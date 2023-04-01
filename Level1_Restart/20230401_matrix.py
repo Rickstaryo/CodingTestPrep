083a13bdac15c180ee441e93ef48ee55e2719975
rickstaryo <qnrud16@gmail.com>
2023-04-01 10:03:18 +0900

    one more

--- Level1_Restart/20230401_matrix.py
+++ Level1_Restart/20230401_matrix.py
@@ -4,3 +4,14 @@ def solution(arr1, arr2):
         for j in range(len(arr1[i])):
             arr1[i][j] = arr1[i][j]+arr2[i][j]
     return answer
+
+
+def solution(arr1, arr2):
+    answer = []
+    for a, b in zip(arr1, arr2):
+        l = []
+        for x, y in zip(a, b):
+            l.append(x+y)
+        answer.append(l)
+
+    return answer
