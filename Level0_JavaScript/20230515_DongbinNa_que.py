1208365d3fe2c5c3541ccf3fb4e9646caa2ff88e
rickstaryo <qnrud16@gmail.com>
2023-05-15 22:14:55 +0900

    Add example of underflow and overflow of Queue

--- Level0_JavaScript/20230515_DongbinNa_que.py
+++ Level0_JavaScript/20230515_DongbinNa_que.py
@@ -1,14 +1,46 @@
 # 파이썬으로 큐를 구현할때는 Collection module에서 제공하는 deque를 이용하자
-from collections import deque
+# from collections import deque
 
-queue = deque()
+# queue = deque()
 
-queue.append(5)
-queue.append(2)
-queue.append(3)
-queue.append(7)
-queue.popleft()
-queue.append(1)
-queue.append(4)
-queue.popleft()
-print(queue)
+# queue.append(5)
+# queue.append(2)
+# queue.append(3)
+# queue.append(7)
+# queue.popleft()
+# queue.append(1)
+# queue.append(4)
+# queue.popleft()
+# print(queue)
+
+
+class Queue:
+    def __init__(self, max_size):
+        self.queue = []
+        self.max_size = max_size
+
+    def enqueue(self, item):
+        if len(self.queue) == self.max_size:
+            print("Overflow!")
+            return False
+        self.queue.append(item)
+        return True
+
+    def dequeue(self):
+        if len(self.queue) == 0:
+            print("Underflow!")
+            return None
+        return self.queue.pop(0)
+
+
+q = Queue(3)
+q.enqueue(1)  # returns True
+q.enqueue(2)  # returns True
+q.enqueue(3)  # returns True
+for element in q.queue:
+    print(q.queue)
+q.enqueue(4)  # prints "Overflow!" and returns False
+q.dequeue()  # returns 1
+q.dequeue()  # returns 2
+q.dequeue()  # returns 3
+q.dequeue()  # prints "Underflow!" and returns None
