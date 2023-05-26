a6d0e1ae68d78bb55f282d89d20389e13f0628ec
rickstaryo <qnrud16@gmail.com>
2023-05-26 17:36:49 +0900

    Bakjun Sorting Question
    1. You need to See what kind of problem on your mind when you have a input problem
    2. Sort is timsort and timsort driven by merge sort and insertion sort which keep indx if there is same value

---
+++ FundamentalofDatastructure/20230526_Bakjun_Algorithm.py
@@ -0,0 +1,13 @@
+# 1. Input set
+N = int(input())
+arr = []
+for i in range(N):
+    age, name = map(str, input().split())
+    age = int(age)
+    arr.append((age, name))
+# 2. Sorting by purpose
+# 2.2 First Sort by Age then Date
+arr.sort(key=lambda x: x[0])
+# 3. Out put set
+for i in arr:
+    print(i[0], i[1])
