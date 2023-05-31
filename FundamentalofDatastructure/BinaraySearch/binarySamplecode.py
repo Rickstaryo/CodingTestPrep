7983b70becb7ee1959f848d5ad6de2a2f2a19856
rickstaryo <qnrud16@gmail.com>
2023-05-31 13:24:27 +0900

    one more solution

--- FundamentalofDatastructure/BinaraySearch/binarySamplecode.py
+++ FundamentalofDatastructure/BinaraySearch/binarySamplecode.py
@@ -39,3 +39,15 @@ for i in x:
         print('yes', end=' ')
     else:
         print('no', end=' ')
+
+# 3. set()
+n = int(input())
+array = set(map(int, input().split()))
+
+m = int(input())
+x = list(map(int, input().split()))
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
