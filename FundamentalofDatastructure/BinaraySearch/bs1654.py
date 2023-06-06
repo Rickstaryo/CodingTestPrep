619620dd23f07050a95caedad0f5264d00acd57d
rickstaryo <qnrud16@gmail.com>
2023-06-06 12:56:47 +0900

    one solution

---
+++ FundamentalofDatastructure/BinaraySearch/bs1654.py
@@ -0,0 +1,23 @@
+# Input
+import sys
+
+k, n = map(int, sys.stdin.readline().split())
+arr = []
+
+for i in range(k):
+    arr.append(int(input()))
+
+# BS
+start = 1
+end = max(arr)
+while start <= end:
+    mid = (start+end)//2
+    cnt = 0
+    for i in range(k):
+        cnt += arr[i] // mid
+    if cnt >= n:
+        start = mid + 1
+    else:
+        end = mid - 1
+# output
+print(end)
