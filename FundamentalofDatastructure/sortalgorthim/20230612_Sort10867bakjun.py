714dd1563ec320082235c2471d6ac3e843d38ca7
rickstaryo <qnrud16@gmail.com>
2023-06-12 15:22:00 +0900

    Editing by one with For-loop

--- FundamentalofDatastructure/sortalgorthim/20230612_Sort10867bakjun.py
+++ FundamentalofDatastructure/sortalgorthim/20230612_Sort10867bakjun.py
@@ -6,3 +6,19 @@ arr.sort()
 
 for i in arr:
     print(i, end=' ')
+
+
+# 2. Using For loop to solve
+# input
+N = int(input())
+arr = list(map(int, input().split()))
+# Eliminating Duplicate by for loop
+d_arr = []
+for i in arr:
+    if i not in d_arr:
+        d_arr.append(i)
+d_arr.sort()
+for i in d_arr:
+    print(i, end=" ")
+
+# Time complexity is So bad compare to Set so i would rather use Set then For-loop
