52c78447ed8e32698f1ee7e8312543f3c0a8e4d6
rickstaryo <qnrud16@gmail.com>
2023-08-07 13:47:44 +0900

    one more mathmatic solution

---
+++ FundamentalofDatastructure/Mathmatics/20230807_1769bakjun.py
@@ -0,0 +1,23 @@
+
+
+# Missing Code
+# #input
+# import sys
+# input=sys.stdin.readline
+# # Function to make x mulitple
+# def xmultiple(num):
+#     s=0
+#     for i in str(num):
+#        s+=int(i)
+#     return s
+# # function
+# N=int(input())
+# cnt=0
+# while N<10:
+#     xmultiple(N)
+#     cnt+=1
+# print(cnt)
+# if N%3==0:
+#     print("YES")
+# else:
+#     print("NO")
