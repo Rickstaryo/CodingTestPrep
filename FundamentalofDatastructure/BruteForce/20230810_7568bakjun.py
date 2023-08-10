96e04c872141235bf9cdcc66b1df19a73ed6a2ee
rickstaryo <qnrud16@gmail.com>
2023-08-10 17:29:11 +0900

    one more solution for Brute force

---
+++ FundamentalofDatastructure/BruteForce/20230810_7568bakjun.py
@@ -0,0 +1,16 @@
+# input
+N = int(input())
+arr = []
+# Brute force
+for _ in range(N):
+    w, h = map(int, input().split())
+    arr.append((w, h))
+
+for i in arr:
+    rank = 1
+    for j in arr:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=" ")
+# output
+# The code is likely resulting in an AttributeError because of a typo in the input().spilt() line. spilt() should be split(). The AttributeError occurs because there is no spilt() method in Python.
